16a3d1deab7e69c4d76a27efff95f5afb9b11ef6
SARAN0209 <108873756+SARAN0209@users.noreply.github.com>
2022-08-12 12:43:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+const videoCardContainer = document.querySelector('.video-container');
+
+let api_key = "403631853862-lf9bo003icmhi63q1f4badukvs4j4tqh.apps.googleusercontent.com";
+let video_http = "https://youtube.googleapis.com/youtube/v3/vedio?";
+let channel_http = "https://youtube.googleapis.com/youtube/v3/channels?";
+let subscriptions_http ="https://youtube.googleapis.com/youtube/v3/subscriptions?"
+let playlists_http = "https://youtube.googleapis.com/youtube/v3/playlists?"
+let activities_http = "https://youtube.googleapis.com/youtube/v3/activities?"
+
+fetch(video_http + new URLSearchParams({
+    key: api_key,
+    part: 'snippet',
+    chart: 'mostPopular',
+    maxResults: 50,
+    regionCode: 'IN'
+}))
+.then(res => res.json())
+.then(data => {
+    data.items.forEach(item => {
+        getChannelIcon(item);
+    })
+})
+.catch(err => console.log(err));
+
+const getChannelIcon = (video_data) => {
+    fetch(channel_http + new URLSearchParams({
+        key: api_key,
+        part: 'snippet',
+        id: video_data.snippet.channelId
+    }))
+    .then(res => res.json())
+    .then(data => {
+        video_data.channelThumbnail = data.items[0].snippet.thumbnails.default.url;
+        makeVideoCard(video_data);
+    })
+}
+
+const makeVideoCard = (data) => {
+    videoCardContainer.innerHTML += `
+    <div class="video" onclick="location.href = 'https://youtube.com/watch?v=${data.id}'">
+        <img src="${data.snippet.thumbnails.high.url}" class="thumbnail" alt="">
+        <div class="content">
+            <img src="${data.channelThumbnail}" class="channel-icon" alt="">
+            <div class="info">
+                <h4 class="title">${data.snippet.title}</h4>
+                <p class="channel-name">${data.snippet.channelTitle}</p>
+            </div>
+        </div>
+    </div>
+    `;
+}
+
+// search bar
+
+const searchInput = document.querySelector('.search-bar');
+const searchBtn = document.querySelector('.search-btn');
+let searchLink = "https://www.youtube.com/results?search_query=";
+
+searchBtn.addEventListener('click', () => {
+    if(searchInput.value.length){
+        location.href = searchLink + searchInput.value;
+    }
+})
+
